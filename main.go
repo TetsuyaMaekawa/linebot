5b7e96c3e8bfb08e5589a480d573839887e90ec0
TetsuyaMaekawa <maekawa@rootship.co.jp>
2019-05-24 16:07:15 +0900

    add log

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/heroku/test/dbaccess/myredis"
 	"github.com/heroku/test/dbaccess/mysql"
 	"github.com/heroku/test/handler"
@@ -10,10 +12,12 @@ func main() {
 	// db接続
 	db, err := mysql.OpenMySQL()
 	if err != nil {
+		log.Print(err)
 		return
 	}
 	rd, err := myredis.OpenRedis()
 	if err != nil {
+		log.Print(err)
 		return
 	}
 	in := handler.InitDB{DB: db, RD: rd}
